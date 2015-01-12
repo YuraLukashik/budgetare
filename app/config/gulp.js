fc66d6bbcda77a8e45bca35138832a4ab9e8b3ad
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2015-01-13 01:44:26 +0300

    removed karma configuration

--- app/config/gulp.js
+++ app/config/gulp.js
@@ -32,10 +32,6 @@ module.exports = {
             "dest": "public/images"
         }
     ],
-    "test": {
-        "configFile": __dirname + '/../../tests/karma.conf.js',
-        "singleRun": true
-    },
     "styles": [
         {
             "src": styles,
